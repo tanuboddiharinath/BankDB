94119165e9fa1391819a019dfd6efb687fc9d92b
tanuboddiharinath <93499759+tanuboddiharinath@users.noreply.github.com>
2022-05-12 12:02:55 +0530

    BankDB 3files

---
+++ seeelect.sql
@@ -0,0 +1,10 @@
+SELECT * FROM BANK_ACCOUNT;
+SELECT * FROM BRANCH;
+SELECT * FROM CUSTOMER;
+SELECT * FROM LOAN;
+SELECT * FROM TRANDETAILS;
+
+select loan_id from loan join bank_account on loan.
+
+
+delete from loan where loan_id = 11;
